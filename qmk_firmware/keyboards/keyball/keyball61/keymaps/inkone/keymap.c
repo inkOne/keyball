5bc6d7ad1ede5188462f5e2a79fec9d1c2deb97c
Frederik Stey <frederik.stey@gmail.com>
2022-05-02 12:02:22 +0200

    force mouse overlay

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/inkone/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/inkone/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , DE_Q     , DE_W     , DE_E     , DE_R     , DE_T     ,                                  DE_Z     , DE_U     , DE_I     , DE_O     , DE_P     , KC_LBRC  ,
     KC_LCTL   , DE_A     , DE_S     , DE_D     , DE_F     , DE_G     ,                                  DE_H     , DE_J     , DE_K     , DE_L     ,LT(1,KC_SCLN)  , KC_QUOT ,
     KC_LSFT    , DE_Y     , DE_X     , DE_C     , DE_V     , DE_B     , LT(2,KC_RBRC)  ,              KC_NUHS, DE_N     , DE_M     , DE_COMM  , DE_DOT   , DE_MINS  , KC_RSFT  ,
-    MO(3)  , MO(1), KC_LCTL  , KC_LALT  , KC_LGUI, KC_SPC,   MO(2),                         LT(3 ,KC_ENT),LT(2,KC_BSPC),_______,KC_RGUI, _______ , KC_RALT  , _______
+    MO(3)  , MO(1), KC_LCTL  , KC_LALT  , KC_LGUI, KC_SPC,   MO(5),                         LT(3 ,KC_ENT),LT(2,KC_BSPC), _______,KC_RGUI, _______ , KC_RALT  , _______
   ),
 
 
@@ -48,14 +48,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    DE_PERC, DE_CIRC, DE_LBRC,   DE_RBRC, _______,  S(KC_8)  ,          S(KC_9)  , KC_PGDN  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______   , _______  , _______  , _______  ,              _______  ,KC_DEL  , _______  , _______  , _______  , _______  , _______
   ),
-
-  [2] = LAYOUT_universal(
-    _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    _______  , _______  , KC_7     , KC_8     , KC_9     , _______  ,                                  _______  , _______  , _______    , _______  , _______  , KC_F12   ,
-    _______  , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                  KC_PGUP  , KC_BTN1  , KC_BTN3  , KC_BTN2  , _______  , _______  ,
-    _______  , _______  , KC_1     , KC_2     , KC_3     ,S(KC_MINS), S(KC_8)  ,            S(KC_9)  , KC_PGDN  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , KC_0     , KC_DOT   , _______  , _______  , _______  ,             _______  ,KC_DEL  , _______  , _______  , _______  , _______  , _______
+[2] = LAYOUT_universal(
+         _______, _______, _______,  _______ ,   _______,  _______,                                       _______  ,    _______, _______,  _______ ,   _______,  _______,
+         _______,    _______, _______,  _______ ,   _______,  _______,                                      _______  ,    _______, _______,  _______ ,   _______,  _______,
+         _______,    _______, _______,  _______ ,   _______,  _______,                                      _______  ,   KC_BTN1  , KC_BTN3  , KC_BTN2,    _______,  _______,
+         _______,    _______, _______,  _______ ,   _______,  _______,  _______  ,               _______  , _______  ,    _______, _______,  _______ ,   _______,  _______,
+         _______, _______  , _______  , _______   , _______  , _______  , _______  ,             _______  , _______  ,   _______, _______,  _______ ,   _______,  _______
   ),
+ // [2] = LAYOUT_universal(
+ //   _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
+ //   _______  , _______  , KC_7     , KC_8     , KC_9     , _______  ,                                  _______  , _______  , _______    , _______  , _______  , KC_F12   ,
+ //  _______  , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                  KC_PGUP  , KC_BTN1  , KC_BTN3  , KC_BTN2  , _______  , _______  ,
+ //  _______  , _______  , KC_1     , KC_2     , KC_3     ,S(KC_MINS), S(KC_8)  ,            S(KC_9)  , KC_PGDN  , _______  , _______  , _______  , _______  , _______  ,
+ //  _______  , _______  , KC_0     , KC_DOT   , _______  , _______  , _______  ,             _______  ,KC_DEL  , _______  , _______  , _______  , _______  , _______
+ // ),
 
  // [3] = LAYOUT_universal(
  //   RGB_TOG  , _______  , _______  , _______  , _______  , _______  ,                                  RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
@@ -67,11 +73,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  // ),
 
   [3] = LAYOUT_universal(
-    RGB_TOG  , _______  , _______  , _______  , _______  , _______  ,                                  KC_MPRV , KC_MPLY,    KC_MNXT, KC_MUTE, KC_VOLD , KC_VOLU ,
-    _______  ,RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  ,  _______ ,                                   KC_HOME  , KC_PAGE_DOWN , KC_PAGE_UP,  KC_END  , KC_BRID,    KC_BRIU,
+    RGB_TOG  , CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  ,  KBC_SAVE ,                                KC_MPRV , KC_MPLY,    KC_MNXT, KC_MUTE, KC_VOLD , KC_VOLU ,
+    _______  ,RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  ,  KBC_RST,                             KC_HOME  , KC_PAGE_DOWN , KC_PAGE_UP,  KC_END  , KC_BRID,    KC_BRIU,
     _______  ,RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  ,                                  KC_LEFT,  KC_DOWN, KC_UP , KC_RGHT,  _______,    _______,
     _______  , _______  , SCRL_DVD , SCRL_DVI , SCRL_MO  , SCRL_TO  , EEP_RST  ,            EEP_RST  , _______  , _______  , _______  , _______ , _______ , _______  ,
-     _______  ,CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE , KBC_RST  ,            _______  , _______  , _______  , _______  , _______ , _______ , _______
+     _______  , _______  , _______  , _______  , _______   ,_______ ,  _______ ,            _______  , _______  , _______  , _______  , _______ , _______ , _______
   ),
 [4] = LAYOUT_universal(
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
@@ -80,8 +86,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , DE_PERC  , DE_PLUS  , DE_LBRC  , DE_RBRC  , DE_TILD  , _______  ,            _______  , DE_AMPR  , DE_LABK  , DE_RABK  , _______  , DE_UNDS  , _______  ,
     _______  , _______  , DE_CIRC  , _______  , _______  , _______  , KC_DEL   ,            KC_INS   , DE_EQL   , XXXXXXX  , XXXXXXX  , XXXXXXX  , DE_BSLS  , _______
   ),
-};
-// clang-format on
+
+  [5] = LAYOUT_universal(
+      _______  ,    _______, _______,  _______ ,   _______,  _______,                                     _______  ,    _______, _______,  _______ ,   _______,  _______,
+      _______  ,    _______, _______,  _______ ,   _______,  _______,                                     _______  ,    _______, _______,  _______ ,   _______,  _______,
+      _______  ,    _______, _______,  _______ ,   _______,  _______,                                     _______  ,   KC_BTN1  , KC_BTN3  , KC_BTN2,    _______,  _______,
+      _______  ,    _______, _______,  _______ ,   _______,  _______,  _______  ,               _______  , _______  ,    _______, _______,  _______ ,   _______,  _______,
+      _______  , _______  , _______  , _______   , _______  , _______  , _______  ,             _______  ,  _______  ,   _______, _______,  _______ ,   _______,  _______
+    )
+};// clang-format on
 
 
 //fooker magic
@@ -105,12 +118,18 @@ const rgblight_segment_t PROGMEM my_layer_conf[] = RGBLIGHT_LAYER_SEGMENTS(
         {0, RGBLED_NUM, HSV_YELLOW}
 );
 
+const rgblight_segment_t PROGMEM my_layer_mouse2[] = RGBLIGHT_LAYER_SEGMENTS(
+        {0, RGBLED_NUM, HSV_RED}
+);
+
+
 const rgblight_segment_t* const PROGMEM my_layers[] = RGBLIGHT_LAYERS_LIST(
     my_layer_caps,
     my_layer_symbols,
     my_layer_mouse,
     my_layer_move,
-    my_layer_conf
+    my_layer_conf,
+    my_layer_mouse2
 );
 
 void keyboard_post_init_user(void) {    // Enable the LED layers
@@ -122,12 +141,13 @@ bool led_update_user(led_t led_state) {
     return true;
 }
 layer_state_t layer_state_set_user(layer_state_t state) {
-    keyball_set_scroll_mode(get_highest_layer(state) == 1);
+    keyball_set_scroll_mode(get_highest_layer(layer_state) == 1);
 
     rgblight_set_layer_state(1, layer_state_cmp(state, 1));
     rgblight_set_layer_state(2, layer_state_cmp(state, 2));
     rgblight_set_layer_state(3, layer_state_cmp(state, 3));
     rgblight_set_layer_state(4, layer_state_cmp(state, 4));
+    rgblight_set_layer_state(5, layer_state_cmp(state, 5));
     return state;
 }
 
@@ -142,7 +162,7 @@ uint32_t mouse_layer_cb(uint32_t trigger_time, void *cb_arg) {
 report_mouse_t pointing_device_task_user(report_mouse_t report) {
     if (report.x != 0 && report.y != 0) {
         layer_on(2);
-	if (mouse_layer_token == INVALID_DEFERRED_TOKEN) {
+	if (mouse_layer_token == INVALID_DEFERRED_TOKEN || get_highest_layer(layer_state) != 5) {
             mouse_layer_token = defer_exec(500, mouse_layer_cb, NULL);
 	} else {
             extend_deferred_exec(mouse_layer_token, 500);
